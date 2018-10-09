ccab0dc8bbf20ddb6b544a8d1b825c5e8913136c
尹宏康 <814696634@qq.com>
2018-10-09 10:31:32 +0800

    ddd

---
+++ src/router/router.js
@@ -0,0 +1,14 @@
+import Vue from "Vue"
+import VueRouter from "vue-router"
+
+import mian from "@/components/main.vue"
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: [
+    {
+      path:'/',component:mian
+    }
+  ]
+})
+
+export default router
