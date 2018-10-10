aa6d262be095625ffa389f727e69b5ec1040ed5e
yhk <yhk@qq.com>
2018-10-10 23:49:30 +0800

    ddd

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import router from "@/router/router.js"
+// import "./assets/css/index.scss"
+import elementUI from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css'
 
-
+Vue.use(elementUI)
 Vue.config.productionTip = false
 
 new Vue({
