9e3141d70338a3a136ba7ad6f980f0048e1a2bbb
yhk <yhk@qq.com>
2018-10-12 00:02:30 +0800

    ddd

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <div class="logo"></div>
+      </el-aside>
       <el-container>
         <el-header>Header</el-header>
         <el-main>Main</el-main>
@@ -13,84 +15,52 @@
 export default {};
 </script>
 <style scoped lang="scss">
-.el-header, .el-footer {
-  background-color: #B3C0D1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
 
-.el-aside {
-  background-color: #D3DCE6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
+.home {
+  height: 100%;
+  background-color: #e5e5e5;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-menu {
+    border-right: 0;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #545c64;
+  }
+  .logo {
+    height: 60px;
+    background: url("../assets/images/tb.jpg");
+    background-size: contain;
+    background-color: white;
+  }
+  .toggle-btn {
+    padding: 0 10px;
+    margin-left: -20px;
+    font-size: 36px;
+    line-height: 60px;
+    color: white;
+    cursor: pointer;
+    &:hover {
+      background-color: #00635a;
+    }
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome {
+    color: white;
+  }
 }
-
-.el-main {
-  background-color: #E9EEF3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
-}
-// .home {
-//   height: 100%;
-//   background-color: #e5e5e5;
-//   .el-menu-admin:not(.el-menu--collapse) {
-//     width: 200px;
-//     min-height: 400px;
-//   }
-//   .el-menu {
-//     border-right: 0;
-//   }
-//   .el-container {
-//     height: 100%;
-//   }
-//   .el-aside {
-//     background-color: #545c64;
-//   }
-//   .el-header {
-//     display: flex;
-//     justify-content: space-between;
-//     align-items: center;
-//     background-color: #545c64;
-//   }
-//   .logo {
-//     height: 60px;
-//     background: url(../assets/logo.png);
-//     background-size: cover;
-//     background-color: white;
-//   }
-//   .toggle-btn {
-//     padding: 0 10px;
-//     margin-left: -20px;
-//     font-size: 36px;
-//     line-height: 60px;
-//     color: white;
-//     cursor: pointer;
-//     &:hover {
-//       background-color: #00635a;
-//     }
-//   }
-//   .system-title {
-//     font-size: 28px;
-//     color: white;
-//   }
-//   .welcome {
-//     color: white;
-//   }
-// }
 </style>
